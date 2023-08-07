ea166db026b1bc4f4475a1a7504ad1cc56a7106f
ShSato4JPN <shsato4.private.1221@gmail.com>
2023-08-08 00:03:39 +0900

    react-paginate  を使ったページネーションボタン制御

--- src/components/Pagination/index.tsx
+++ src/components/Pagination/index.tsx
@@ -1,18 +1,38 @@
 import { Link } from "react-router-dom";
+import ReactPaginate from "react-paginate";
+import { useNavigate } from "react-router-dom";
 
 type PaginationProps = {
   currentPage: number;
 };
 
 function Pagination({ currentPage }: PaginationProps) {
+  // 最大ページ数（仮）
+  const totalCount = 10;
+  const navigate = useNavigate();
+
   return (
     <>
-      <Link to={`/?page=${currentPage - 1}`}>
+      {/* <Link to={`/?page=${currentPage - 1}`}>
         <div>prev</div>
       </Link>
       <Link to={`/?page=${currentPage + 1}`}>
         <div>next</div>
-      </Link>
+      </Link> */}
+      <ReactPaginate
+        pageCount={totalCount}
+        pageRangeDisplayed={2}
+        marginPagesDisplayed={2}
+        previousLabel={"prev"}
+        nextLabel={"next"}
+        breakLabel={"..."}
+        breakClassName={"break-me"}
+        containerClassName={"pagination"}
+        activeClassName={"active"}
+        onPageChange={(page) => {
+          navigate(`/?page=${page.selected + 1}`);
+        }}
+      />
     </>
   );
 }
