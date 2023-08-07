2eb06f296c07bad784029313ece132d637c22d87
ShSato4JPN <shsato4.private.1221@gmail.com>
2023-08-08 01:48:45 +0900

    ページネーションのボタン制御を実装

--- src/components/Pagination/index.tsx
+++ src/components/Pagination/index.tsx
@@ -1,25 +1,62 @@
+import styles from "./style.module.css";
 import { Link } from "react-router-dom";
 import ReactPaginate from "react-paginate";
 import { useNavigate } from "react-router-dom";
+import { useMemo } from "react";
 
 type PaginationProps = {
   currentPage: number;
 };
 
+type FilterParams = {
+  showNumbers: number[];
+  leftBreak: boolean;
+  rightBreak: boolean;
+};
+
 function Pagination({ currentPage }: PaginationProps) {
   // 最大ページ数（仮）
-  const totalCount = 10;
   const navigate = useNavigate();
+
+  const totalCount: number = 10;
+  const pageNumbers = useMemo<FilterParams>(() => {
+    const numbers = Array.from({ length: totalCount }).map((_, i) => i + 1);
+
+    let leftBreak = true;
+    const min = Math.min(...numbers);
+    if (numbers.includes(min + 1)) {
+      leftBreak = false;
+    }
+
+    let rightBreak = true;
+    const max = Math.max(...numbers);
+    if (numbers.includes(max + 1)) {
+      rightBreak = false;
+    }
+
+    const showNumbers = numbers
+      .filter(
+        (n) =>
+          n == 1 ||
+          n === totalCount ||
+          n === currentPage ||
+          n === currentPage - 1 ||
+          n === currentPage + 1 ||
+          (currentPage === totalCount && n === currentPage - 2) ||
+          (currentPage === 1 && n === currentPage + 2)
+      )
+      .sort((a, b) => a - b);
+
+    return {
+      showNumbers,
+      leftBreak,
+      rightBreak,
+    };
+  }, [currentPage]);
 
   return (
     <>
-      {/* <Link to={`/?page=${currentPage - 1}`}>
-        <div>prev</div>
-      </Link>
-      <Link to={`/?page=${currentPage + 1}`}>
-        <div>next</div>
-      </Link> */}
-      <ReactPaginate
+      {/* <ReactPaginate
         pageCount={totalCount}
         pageRangeDisplayed={2}
         marginPagesDisplayed={2}
@@ -32,7 +69,21 @@ function Pagination({ currentPage }: PaginationProps) {
         onPageChange={(page) => {
           navigate(`/?page=${page.selected + 1}`);
         }}
-      />
+      /> */}
+
+      <ul>
+        {pageNumbers.showNumbers.map((n) => (
+          <li key={n}>
+            <Link to={`/?page=${n}`}>
+              {n === currentPage ? (
+                <div className={`${styles.box} ${styles.isCurrent}`}>{n}</div>
+              ) : (
+                <div className={styles.box}>{n}</div>
+              )}
+            </Link>
+          </li>
+        ))}
+      </ul>
     </>
   );
 }
